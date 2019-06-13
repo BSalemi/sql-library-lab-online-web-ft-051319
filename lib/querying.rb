c9e671b8617aa4ae18e12efa8471aa8fa4a61dae
Brian Salemi <briandsalemi@gmail.com>
2019-06-13 21:08:07 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,6 +35,7 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT name, COUNT (book_id) FROM characters
   JOIN character_books
   ON characters.id = character_books.character_id
+  GROUP BY name
   ORDER BY COUNT(*) DESC"
 
 end
