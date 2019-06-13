9c352e8ca8e10e09746c2219e57f69307659c950
Brian Salemi <briandsalemi@gmail.com>
2019-06-13 21:05:33 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT character_books.character_id, COUNT(book_id)
+  "SELECT character_books.character.name, COUNT(book_id)
   FROM character_books"
 
 end
